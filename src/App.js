be89cd43f5e62ccca31f85fe48d48652342cda7e
icaoktavianita <annisadwioktavianita@gmail.com>
2017-03-16 11:54:48 +0700

    menu contact

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+        <div className="all">
+          <div className="navbar">
+            <a className="menu" href="#">Menu</a>
+            <a className="contact" href="#">Contact</a>
+          </div>
+          <p>
+          </p>
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Nav.css';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="Nav">
+        <img src=".img/logo.png"></img>
+      </div>
+    );
+  }
+}
+
+export default Nav;
